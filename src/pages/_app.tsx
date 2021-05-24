9e94086456cea3d41afe3de9f1fe2bd36d503174
Paulo Venones <paulovenones@MacBook-Pro-de-Paulo.local>
2021-05-24 11:50:54 -0300

    fix tailwind dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppProps } from 'next/app';
 
 import { ThemeProvider } from 'styled-components';
-import '../styles/globals.css';
+import '../styles/global.css';
 import theme from '../styles/theme';
 // import 'tailwindcss/tailwind.css';
 
